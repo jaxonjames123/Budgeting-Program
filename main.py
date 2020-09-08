2332cb43ce18c00fad835546819cfdad926c79fe
Jaxon Terrell <terrellj8@students.rowan.edu>
2020-09-08 15:12:12 -0400

    added a main.py file
    my_budget now only contains the functions used in main method, along with the enum states.
    also did some exception handling on the goals functionality.

---
+++ main.py
@@ -0,0 +1,40 @@
+from my_budget import get_goals, edit_accounts, get_monthly_income, get_bill_amounts, print_bills, deposit, MyStates
+
+def main():
+    states = MyStates
+    print('Welcome to Jaxon\'s Budget Program!')
+    for state in states:
+        print(f'{state.value}: {state.name.replace("_", " ").title()}')
+    try:
+        current_state = input('What would you like to do?\n')
+        current_state = int(current_state)
+    except ValueError:
+        print(f'Value Error: could not convert {current_state} to int, please enter a number '
+              f'corresponding to a menu item')
+    while True:
+        if current_state == states.goals.value:
+            get_goals()
+        elif current_state == states.accounts.value:
+            edit_accounts()
+        elif current_state == states.monthly_income.value:
+            get_monthly_income()
+        elif current_state == states.bills.value:
+            print_bills(get_bill_amounts())
+        elif current_state == states.make_deposit.value:
+            deposit()
+        if current_state == states.exit_program.value:
+            print(f'Exiting program... Have a nice day!')
+            break
+        print('')
+        for state in states:
+            print(f'{state.value}: {state.name.replace("_", " ").title()}')
+        try:
+            current_state = input('What would you like to do?\n')
+            current_state = int(current_state)
+        except ValueError:
+            print(f'Value Error: could not convert {current_state} to int, please enter a number '
+                  f'corresponding to a menu item')
+
+
+if __name__ == "__main__":
+    main()
--- my_budget.py
+++ my_budget.py
@@ -110,14 +110,21 @@ def get_goals():
             break
     else:
         # Get goals from user
-        n = int(input('How many budgeting goals do you have?: '))
-        goals = []
-        for i in range(n):
-            goals.append(input(f'What is your #{i + 1} goal?: ').upper() + '\n')
-            i += 1
-        with open(filename, 'w') as f:
-            f.writelines(goals)
-        print_file(filename)
+        n = input('How many budgeting goals do you have?: ')
+        try:
+            n = int(n)
+            if n < 0:
+                print('Please enter a whole number greater than 0\n')
+            else:
+                goals = []
+                for i in range(n):
+                    goals.append(input(f'What is your #{i + 1} goal?: ').upper() + '\n')
+                    i += 1
+                with open(filename, 'w') as f:
+                    f.writelines(goals)
+                print_file(filename)
+        except ValueError:
+            print("Please enter a whole number greater than 0\n")
 
 
 def get_monthly_income():
@@ -232,41 +239,4 @@ class MyStates(Enum):
     bills = auto()
 
 
-def main():
-    states = MyStates
-    print('Welcome to Jaxon\'s Budget Program!')
-    for state in states:
-        print(f'{state.value}: {state.name.replace("_", " ").title()}')
-    try:
-        current_state = input('What would you like to do?\n')
-        current_state = int(current_state)
-    except ValueError:
-        print(f'Value Error: could not convert {current_state} to int, please enter a number '
-              f'corresponding to a menu item')
-    while True:
-        if current_state == states.goals.value:
-            get_goals()
-        elif current_state == states.accounts.value:
-            edit_accounts()
-        elif current_state == states.monthly_income.value:
-            get_monthly_income()
-        elif current_state == states.bills.value:
-            print_bills(get_bill_amounts())
-        elif current_state == states.make_deposit.value:
-            deposit()
-        if current_state == states.exit_program.value:
-            print(f'Exiting program... Have a nice day!')
-            break
-        print('')
-        for state in states:
-            print(f'{state.value}: {state.name.replace("_", " ").title()}')
-        try:
-            current_state = input('What would you like to do?\n')
-            current_state = int(current_state)
-        except ValueError:
-            print(f'Value Error: could not convert {current_state} to int, please enter a number '
-                  f'corresponding to a menu item')
 
-
-if __name__ == "__main__":
-    main()
