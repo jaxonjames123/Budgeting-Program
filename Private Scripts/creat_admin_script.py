1cc24e25e0501455caa6cd43540a54d8c28854bd
Jaxon Terrell <terrellj8@students.rowan.edu>
2020-09-10 15:36:24 -0400

    Delete creat_admin_script.py

--- Private Scripts/creat_admin_script.py
+++
@@ -1,8 +0,0 @@
-from security import encrypt_password
-import json
-
-filename = './AdminLogin.json'
-username = 'noxaj123'
-password = encrypt_password('hhrS15513')
-with open(filename, 'w') as f:
-    json.dump({username: password}, f)
